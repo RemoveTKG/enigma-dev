681242c93bdd0b373c2a7752b690411fe0450c85
RemoveTKG <46246081+RemoveTKG@users.noreply.github.com>
2019-01-01 20:19:12 -0500

    Revert "Add final slash to directory constants (#1464)"
    This reverts commit ecfad6bab27a12398456faeabd0b3c8de0353755.

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -9,33 +9,26 @@
 using std::string;
 
 namespace enigma {
-  
-static inline string add_slash(const string& dir) {
-  if (dir.empty() || *dir.rbegin() != '/') return dir + '/';
-  return dir;
-}
-  
 void initialize_directory_globals() {
   // Set the working_directory
   char buffer[PATH_MAX + 1];
   if (getcwd(buffer, PATH_MAX + 1) != NULL)
-    enigma_user::working_directory = add_slash(buffer);
+    enigma_user::working_directory = buffer;
 
   // Set the program_directory
   buffer[0] = 0;
   ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX + 1);
   if (count !=  -1)
-    enigma_user::program_directory = add_slash(dirname(buffer));
+    enigma_user::program_directory = dirname(buffer) + string("/");
 
   // Set the temp_directory
-  char *env = getenv("TMPDIR");
+  char const *env = getenv("TMPDIR");
 
-  if (env != NULL)
-    enigma_user::temp_directory = add_slash(env);
-  else
-    enigma_user::temp_directory = "/tmp/";
+  if (env == 0)
+    env = "/tmp/";
+
+  enigma_user::temp_directory = env; 
 }
-
 }
 
 int main(int argc, char** argv) {
